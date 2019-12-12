5053c36151828ed2df9824a5bba43ba6c2e35987
RINGLING <41229376+GtZero@users.noreply.github.com>
2019-12-12 19:30:05 +0800

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,11 +1,5 @@
 public class GitTest {
 
-    private String str1;
-    private String str2;
-
-    private String str5;
-    private String str6;
-
-    private String str3;
-    private String str4;
+    private String str7;
+    private String str8;
 }
