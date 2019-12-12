12ce3151d06f43db6f19a57e55343fc33141db8d
RINGLING <892062214@qq.com>
2019-12-12 19:46:27 +0800

    feat new5

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,14 +6,17 @@ public class GitTest {
     private String str2;
 
     public void test(){
-        System.out.println("Git test");
+        System.out.println("Git test99");
     }
 
     public void test2(){
-        System.out.println("Git test3");
+        System.out.println("Git test6");
     }
 
     public void test3(){
+
+    }
+    public void test4(){
 
     }
 }
