ef507a53ac5cbf1cc733e40f39c991cd4add1e20
RINGLING <892062214@qq.com>
2019-12-12 19:21:04 +0800

    feat new

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,11 @@
 public class GitTest {
-    
+
+    private String str1;
+    private String str2;
+
+    private String str5;
+    private String str6;
+
     private String str3;
     private String str4;
 }
